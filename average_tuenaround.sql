6b1cd090f7298b68afd8970385fedd12ed8a5c89
Prarinam Deria <prarinamderia2000@gmail.com>
2025-10-26 21:23:15 +0530

    feat: entire db name added in average_turnaround.sql file

--- average_tuenaround.sql
+++ average_tuenaround.sql
@@ -12,9 +12,9 @@ start_time DATETIME2,
 end_time DATETIME2 
 );
 
-INSERT INTO drivers (driver_id, driver_name) VALUES (401, 'Rajesh'), (402, 'Neha');
+INSERT INTO EMPLOYEE.dbo.drivers (driver_id, driver_name) VALUES (401, 'Rajesh'), (402, 'Neha');
 
-INSERT INTO rides (ride_id, driver_id, start_time, end_time)
+INSERT INTO EMPLOYEE.dbo.rides (ride_id, driver_id, start_time, end_time)
 VALUES
   (501, 401, '2025-01-01 10:00:00', '2025-01-01 10:30:00'),
   (502, 401, '2025-01-01 11:00:00', '2025-01-01 11:25:00'),
@@ -22,9 +22,9 @@ VALUES
   (504, 402, '2025-01-01 09:00:00', '2025-01-01 09:45:00'),
   (505, 402, '2025-01-01 10:30:00', '2025-01-01 11:00:00');
 
-SELECT * FROM drivers;
+SELECT * FROM EMPLOYEE.dbo.drivers;
 
-SELECT * FROM rides;
+SELECT * FROM EMPLOYEE.dbo.rides;
 
 
 -- GET the Average Turn Around time
@@ -32,12 +32,12 @@ SELECT * FROM rides;
 WITH rides_gaps AS (
 SELECT driver_id, start_time, 
 LAG(end_time) OVER (PARTITION BY driver_id ORDER BY start_time) as prev_end
-FROM rides
+FROM EMPLOYEE.dbo.rides
 )
 
 SELECT d.driver_id, d.driver_name, ROUND(AVG(DATEDIFF(MINUTE, prev_end, start_time)), 2) as avg_gap_minutes
 FROM rides_gaps rg
-JOIN drivers d
+JOIN EMPLOYEE.dbo.drivers d
 ON d.driver_id = rg.driver_id
 GROUP BY d.driver_id, d.driver_name
 ORDER BY avg_gap_minutes ASC;
@@ -48,4 +48,4 @@ ORDER BY avg_gap_minutes ASC;
 SELECT ride_id, driver_id, 
 MONTH(start_time) as month,
 YEAR(start_time) as year
-FROM rides;
+FROM EMPLOYEE.dbo.rides;
--- consecutive_row.sql
+++ consecutive_row.sql
@@ -1,1 +1,4 @@
-Write a query to produce the start and end date
+-- Write a query to produce the start and end date for each consecutive run of the same event
+-- Consecutive means the same event occurs in sequential rows ordered by date
+-- If a run contains only one row , set start date = end_date
+
